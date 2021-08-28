443b422c1677a395d7495f1984f37d93c7f97f01
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-28 15:44:32 -0700

    added drawer class

--- dungeon.py
+++ dungeon.py
@@ -1,12 +1,23 @@
+import curses
+import random
+
+
 class Room:
     def __init__(self, left, right, top, bottom):
         self.left, self.right, self.top, self.bottom = left, right, top, bottom
 
     def split(self):
         left, right, top, bottom = self.left, self.right, self.top, self.bottom
-        room_left = Room(left, (left + right) / 2, top, bottom)
-        room_right = Room((left + right) / 2, right, top, bottom)
-        return (room_left, room_right)
+        max = 5
+        rand = random.randint(2, max + 1)
+        direction = random.randint(0, 2)
+        if direction < 1:
+            room_a = Room(left, (left + right) / rand, top, bottom)
+            room_b = Room((left + right) / rand, right, top, bottom)
+        else:
+            room_a = Room(left, right, top, (top + bottom) / rand)
+            room_b = Room(left, right, (top + bottom) / rand, bottom)
+        return (room_a, room_b)
 
     def __repr__(self):
         return (
@@ -37,7 +48,23 @@ class Generator:
         for room in self.rooms:
             print(str(room))
 
-room = Room(0, 10, 0, 10)
+
+class Drawer:
+    def __init__(self, resolution):
+        self.resolution = resolution
+
+    def draw(self, rooms):
+        stdscr = curses.initscr()
+        for room in rooms:
+            r = self.resolution
+            for x in range(int(room.left / r), int(room.right / r) - 1):
+                for y in range(int(room.bottom / r), int(room.top / r) - 1):
+                    stdscr.addch(y, x, "#")
+        stdscr.refresh()
+
+
+room = Room(0, 30, 10, 0)
 gen = Generator()
 gen.generate(room, 3, 0)
-gen.print()
+dr = Drawer(0.25)
+dr.draw(gen.rooms)
