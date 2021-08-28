c6daeadcaf4e11fcca2580be1ceb56e4a3739283
Kon Aoki <kon.aoki.us@gmail.com>
2021-08-28 15:03:48 -0700

    added generator class

--- dungeon.py
+++ dungeon.py
@@ -19,3 +19,25 @@ class Room:
             + str(self.bottom)
         )
 
+
+class Generator:
+    def __init__(self):
+        self.rooms = []
+
+    def generate(self, room, max_depth, depth):
+        if depth >= max_depth:
+            self.rooms.append(room)
+            return
+        depth += 1
+        rooms = room.split()
+        self.generate(rooms[0], max_depth, depth)
+        self.generate(rooms[1], max_depth, depth)
+
+    def print(self):
+        for room in self.rooms:
+            print(str(room))
+
+room = Room(0, 10, 0, 10)
+gen = Generator()
+gen.generate(room, 3, 0)
+gen.print()
